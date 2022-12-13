e1ff57912c1733c35261fb4614089d20fb2027a9
NuzhathBegum <120133162+NuzhathBegum@users.noreply.github.com>
2022-12-14 05:23:31 +0530

    Add files via upload

---
+++ StudentBluePrint.java
@@ -0,0 +1,44 @@
+package com.edubridge1.com.practice;
+
+public class StudentBluePrint {
+	private int rollNo;
+	private String studentName;
+	private String favSub;
+	private int totalMarks;
+	private int moblileNo;
+	public int getRollNo() {
+		return rollNo;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public String getFavSub() {
+		return favSub;
+	}
+	public int getTotalMarks() {
+		return totalMarks;
+	}
+	public int getMoblileNo() {
+		return moblileNo;
+	}
+	public void setRollNo(int rollNo) {
+		this.rollNo = rollNo;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public void setFavSub(String favSub) {
+		this.favSub = favSub;
+	}
+	public void setTotalMarks(int totalMarks) {
+		this.totalMarks = totalMarks;
+	}
+	public void setMoblileNo(int moblileNo) {
+		this.moblileNo = moblileNo;
+	}
+	
+
+}
+
+
+
